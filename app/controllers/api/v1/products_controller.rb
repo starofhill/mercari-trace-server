2e92bbbba99358f75ae51dbf1ce53ff2b9283407
masa_fuk <purizun7610@gmail.com>
2020-09-23 11:32:33 +0900

    products controller

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render json: @product
+  end
+end
