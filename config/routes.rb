ac79aa805b590739e49fb054a3dba7ac73958094
masa_fuk <purizun7610@gmail.com>
2020-10-11 19:46:03 +0900

    route comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1' do
-      resources :products
+      resources :products do
+        resources :comments
+      end
     end
   end
 end
