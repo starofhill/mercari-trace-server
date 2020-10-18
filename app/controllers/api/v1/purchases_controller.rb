d7c1d5499dabad2c3b80b29a23ad573beb4f4d1a
masa_fuk <purizun7610@gmail.com>
2020-10-18 22:25:33 +0900

    purchaseコントローラ作成

---
+++ app/controllers/api/v1/purchases_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::PurchasesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @purchase = Purchase.all
+    render json: @purchase
+  end
+
+  def create
+    product = Product.find_by(id: purchase_params[:product_id])
+
+    if product.present? && product.status === "sale"
+      @purchase = product.purchases.build(purchase_params)
+      @purchase.user_id = current_user.id
+
+      if ["convenience", "docomo", "au", "softbank"].include?((purchase_params[:payment_method]))
+        @purchase.total_price = product.price + 100
+      else
+        @purchase.total_price = product.price
+      end
+    else
+      render json: {message: "Product not found"}, status: :not_found and return
+    end
+
+    if @purchase.save
+      product.update(status: "sold")
+      render json: @purchase
+    end
+  end
+
+  private
+    def purchase_params
+      params.require(:purchase).permit(:product_id, :payment_method)
+    end
+
+end
